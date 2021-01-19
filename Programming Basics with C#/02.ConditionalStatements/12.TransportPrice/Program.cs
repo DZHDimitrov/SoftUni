8fe3af66a91829c541f579d7ac260435b58cc23e
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-01-19 15:06:02 +0200

    Added ConditionalStatements - exercises

---
+++ Programming Basics with C#/02.ConditionalStatements/12.TransportPrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12.TransportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programming Basics with C#/02.ConditionalStatements/17.FuelTank-Part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17.FuelTank_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
