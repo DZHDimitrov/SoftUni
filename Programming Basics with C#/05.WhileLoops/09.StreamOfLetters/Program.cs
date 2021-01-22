d88fac3684ce6b0c60bc97eab786ff396b9b5a74
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-01-22 23:10:31 +0200

    Added WhileLoops - exercises

---
+++ Programming Basics with C#/05.WhileLoops/09.StreamOfLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.StreamOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Programming Basics with C#/05.WhileLoops/10.NumbersDividedBy3WithoutReminder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10.NumbersDividedBy3WithoutReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
