cf7d244a2d99316f026a4c5a8beee8e277df01b1
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-01 17:02:53 +0200

    Added firstProblem from Arrays-Exercises

---
+++ Programming Fundamentals with C#/04. Arrays - Exercises/01.Train/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] array = new int[wagons];
+            for (int i = 0; i < wagons; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                array[i] = people;
+            }
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
