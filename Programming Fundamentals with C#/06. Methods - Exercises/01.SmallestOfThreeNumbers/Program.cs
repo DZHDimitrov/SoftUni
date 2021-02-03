cad6bd009091c34617ef6d04993008e2d92f10fb
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-03 17:43:42 +0200

    Added Methods - Exercises

---
+++ Programming Fundamentals with C#/06. Methods - Exercises/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            SmallestNumer(array);
+        }
+        static void SmallestNumer(int[] array)
+        {
+            Console.WriteLine(array.Min());
+        }
+    }
+}
