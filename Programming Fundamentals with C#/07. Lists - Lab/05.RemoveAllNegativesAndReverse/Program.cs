5dd30fbae481ab38d44fe80b47b68d2ddc290509
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-04 23:18:35 +0200

    Added Lists - Lab

---
+++ Programming Fundamentals with C#/07. Lists - Lab/05.RemoveAllNegativesAndReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveAllNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            numbers = numbers.Where(x => x > 0).Reverse().ToList();
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/07. Lists - Lab/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_Manipulation_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] data = input.Split();
+                string command = data[0];
+                switch (command)
+                {
+                    case "Add":
+                        integers.Add(int.Parse(data[1]));
+                        break;
+                    case "Remove":
+                        integers.Remove(int.Parse(data[1]));
+                        break;
+                    case "RemoveAt":
+                        integers.RemoveAt(int.Parse(data[1]));
+                        break;
+                    case "Insert":
+                        integers.Insert(int.Parse(data[2]), int.Parse(data[1]));
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
