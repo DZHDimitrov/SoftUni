1d3db0285921718bf42b1ec2af71aa2df02bf54b
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-04 23:14:44 +0200

    Added Lists - Lab

---
+++ Programming Fundamentals with C#/07. Lists - Exercise/03.MergingLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbersTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> overallList = new List<int>();
+
+            for (int i = 0; i < Math.Min(numbersOne.Count, numbersTwo.Count); i++)
+            {
+                overallList.Add(numbersOne[i]);
+                overallList.Add(numbersTwo[i]);
+                numbersOne.RemoveAt(i);
+                numbersTwo.RemoveAt(i--);
+
+                if (numbersOne.Count == 0 || numbersTwo.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (numbersOne.Count != numbersTwo.Count)
+            {
+                for (int i = 0; i < Math.Max(numbersOne.Count, numbersTwo.Count); i++)
+                {
+                    if (numbersOne.Count > 0)
+                    {
+                        overallList.Add(numbersOne[i]);
+                    }
+                    else if (numbersTwo.Count > 0)
+                    {
+                        overallList.Add(numbersTwo[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", overallList));
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/07. Lists - Exercise/04.ListOfProducts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productsNumber = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < productsNumber; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+            int index = 1;
+            foreach (var item in products.OrderBy(x => x))
+            {
+                Console.WriteLine($"{index}.{item}");
+                index++;
+            }
+        }
+
+    }
+}
