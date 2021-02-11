abb40e7241eacb99fe0d549f5437325b70545f30
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-11 22:03:52 +0200

    Added ObjectsAndClasses - Exercises

---
+++ Programming Fundamentals with C#/10. ObjectsAndClasses - Exercises/07.OrderByAge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> people = new List<Person>();
+
+            while (input != "End")
+            {
+                string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = data[0];
+                string ID = data[1];
+                int age = int.Parse(data[2]);
+                Person person = new Person(name, ID, age);
+
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+            foreach (var item in people.OrderBy(x=>x.Age))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+    class Person
+    {
+        public Person(string name, string iD, int age)
+        {
+            Name = name;
+            ID = iD;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+}
