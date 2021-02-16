0d31e264e78d5eca4c76b8826bc46353b41c0326
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-16 21:50:34 +0200

    Added Associative Arrays - Exercise

---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/04.Orders/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> allProducts = new List<Product>();
+
+            string input = Console.ReadLine();
+
+            while (input != "buy")
+            {
+                string[] cmdArg = input.Split();
+                string productName = cmdArg[0];
+                double productPrice = double.Parse(cmdArg[1]);
+                int productQuantity = int.Parse(cmdArg[2]);
+
+                Product product = new Product(productName, productPrice, productQuantity);
+                if (allProducts.Any(x=>x.Name == productName))
+                {
+                    allProducts = ModifyCollection(allProducts, product);
+                }
+                else
+                {
+                    allProducts.Add(product);
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in allProducts)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        static List<Product> ModifyCollection(List<Product> list,Product product)
+        {
+            foreach (var item in list)
+            {
+                if (item.Name == product.Name)
+                {
+                    item.Quantity += product.Quantity;
+                    if (item.Price != product.Price)
+                    {
+                        item.Price = product.Price;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+    class Product
+    {
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {Price * Quantity:F2}";
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/05.SoftUniParking/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Parking parking = new Parking();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                string command = cmdArgs[0];
+                string name = cmdArgs[1];
+                try
+                {
+                    switch (command)
+                    {
+                        case "register":
+                            string number = cmdArgs[2];
+                            Person person = new Person(name, number);
+                            parking.Register(person);
+                            break;
+                        case "unregister":
+                            parking.Unregister(name);
+                            break;
+                    }
+                }
+                catch (Exception ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+            foreach (var item in parking.People)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Person
+    {
+        public Person(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} => {Number}";
+        }
+    }
+    class Parking
+    {
+        public Parking()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void Register(Person person)
+        {
+            if (People.Any(x => x.Name == person.Name))
+            {
+                Person lastRegister = People.FirstOrDefault(x => x.Name == person.Name);
+                throw new ArgumentException($"ERROR: already registered with plate number {lastRegister.Number}");
+            }
+            else
+            {
+                People.Add(person);
+                Console.WriteLine($"{person.Name} registered {person.Number} successfully");
+            }
+        }
+        public void Unregister(string name)
+        {
+            if (!People.Any(x => x.Name == name))
+            {
+                throw new ArgumentException($"ERROR: user {name} not found");
+            }
+            else
+            {
+                Person person = People.FirstOrDefault(x => x.Name == name);
+                People.Remove(person);
+                Console.WriteLine($"{person.Name} unregistered successfully");
+            }
+        }
+    }
+}
