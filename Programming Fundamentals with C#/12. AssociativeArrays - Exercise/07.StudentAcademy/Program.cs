b08cf950b20c159f3f00d88fcff9bf6ca9f5b956
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-18 22:10:06 +0200

    Added Associative Arrays - Exercise

---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/07.StudentAcademy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+                students[name].Add(grade);
+            }
+            foreach (var student in students.OrderByDescending(x=>x.Value.Average()).Where(x=>x.Value.Average() >= 4.50))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():F2}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/08.CompanyUsers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<string>> companies = new Dictionary<string, HashSet<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] cmdArgs = input.Split(" -> ");
+                string companyName = cmdArgs[0];
+                string id = cmdArgs[1];
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new HashSet<string>());
+                }
+                companies[companyName].Add(id);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in companies.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine(company.Key);
+                foreach (var id in company.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
