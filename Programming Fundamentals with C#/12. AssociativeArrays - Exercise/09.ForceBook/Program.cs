fd161962ac0f82416246bdeba1d993ff28d785d9
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-02-20 22:16:46 +0200

    Added Associative Arrays - Exercise

---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/09.ForceBook/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> forceSides = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Lumpawaroo")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string forceSide;
+                string forceUser;
+                string modifyArray;
+
+                if (cmdArgs.Contains("|"))
+                {
+                    modifyArray = string.Join(" ", cmdArgs);
+                    cmdArgs = modifyArray.Split("|", StringSplitOptions.RemoveEmptyEntries);
+                    forceSide = cmdArgs[0].TrimEnd(' ');
+                    forceUser = cmdArgs[1].TrimStart(' ');
+                    if (!forceSides.ContainsKey(forceSide))
+                    {
+                        forceSides.Add(forceSide, new List<string>());
+                    }
+
+                    if (!forceSides[forceSide].Contains(forceUser) && !forceSides.Values.Any(x => x.Contains(forceUser)))
+                    {
+                        forceSides[forceSide].Add(forceUser);
+                    }
+                }
+                else if (cmdArgs.Contains("->"))
+                {
+                    modifyArray = string.Join(" ", cmdArgs);
+                    cmdArgs = modifyArray.Split("->", StringSplitOptions.RemoveEmptyEntries);
+                    forceUser = cmdArgs[0].TrimEnd(' ');
+                    forceSide = cmdArgs[1].TrimStart(' ');
+
+                    if (!forceSides.Any(x => x.Value.Contains(forceUser)))
+                    {
+                        if (!forceSides.ContainsKey(forceSide))
+                        {
+
+                            forceSides.Add(forceSide, new List<String>());
+                        }
+
+                        forceSides[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+                    else
+                    {
+                        foreach (var s in forceSides)
+                        {
+                            if (s.Value.Contains(forceUser))
+                            {
+                                s.Value.Remove(forceUser);
+                            }
+                        }
+
+                        if (forceSides.ContainsKey(forceSide) == false)
+                        {
+                            forceSides.Add(forceSide, new List<string>());
+                        }
+                        forceSides[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in forceSides.Where(x => x.Value.Count > 0).OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+
+                Console.WriteLine($"Side: {item.Key}, Members: {item.Value.Count}");
+                foreach (var forceUser in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {forceUser}");
+                }
+
+            }
+        }          
+    }
+}
---
+++ Programming Fundamentals with C#/12. AssociativeArrays - Exercise/10.SoftUniExamResults/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.SoftUniExamResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            Dictionary<string, int> languageSub = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "exam finished")
+            {
+                string[] cmdArg = input.Split('-');
+                string name = cmdArg[0];
+                if (cmdArg.Contains("banned"))
+                {
+                    students.Remove(name);
+                    input = Console.ReadLine();
+                    continue;
+                }
+                string language = cmdArg[1];
+                int points = int.Parse(cmdArg[2]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, points);
+                }
+                else if (students.ContainsKey(name) && students[name] < points)
+                {
+                    students[name] = points;
+                }
+                if (!languageSub.ContainsKey(language))
+                {
+                    languageSub.Add(language,0);
+                }
+                languageSub[language]++;
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Results:");
+            foreach (var student in students.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{student.Key} | {student.Value}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var submission in languageSub.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{submission.Key} - {submission.Value}");
+            }
+        }
+    }
+}
