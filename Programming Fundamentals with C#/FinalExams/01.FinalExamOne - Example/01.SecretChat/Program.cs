c4a591066274f6d23e70f5397e268b21c1f1446c
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-03-14 20:39:08 +0200

    Added FinalExamOne - Exercise

---
+++ Programming Fundamentals with C#/FinalExams/01.FinalExamOne - Example/01.SecretChat/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ThePianist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != "Reveal")
+            {
+                string[] array = input.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+                string command = array[0];
+
+                switch (command)
+                {
+                    case "InsertSpace":
+                        int index = int.Parse(array[1]);
+                        text = text.Insert(index, " ");
+                        Console.WriteLine(text);
+                        break;
+                    case "Reverse":
+                        string givenText = array[1];
+                        if (text.Contains(givenText))
+                        {
+                            int indexoftext = text.IndexOf(givenText);
+                            string substring = text.Substring(indexoftext, givenText.Length);
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = substring.Length - 1; i >= 0; i--)
+                            {
+                                sb.Append(substring[i]);
+                            }
+                            string reversed = sb.ToString();
+
+                            text = text.Remove(indexoftext, substring.Length);
+                            text = text.Insert(text.Length, reversed);
+                            Console.WriteLine(text);
+                        }
+                        else
+                        {
+                            Console.WriteLine("error");
+                        }
+                        break;
+
+                    case "ChangeAll":
+                        string partOfText = array[1];
+                        string replacement = array[2];
+                        if (text.Contains(partOfText))
+                        {
+                            text = text.Replace(partOfText, replacement);
+                            Console.WriteLine(text);
+                        }
+                        else
+                        {
+                            Console.WriteLine("error");
+                        }
+                        break;
+
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"You have a new text message: {text}");
+        }
+
+    }
+}
---
+++ Programming Fundamentals with C#/FinalExams/01.FinalExamOne - Example/02.MirrorWords/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ThePianist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([#@])([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(text);
+            List<string> validMatches = new List<string>();
+            if (matches.Count > 0)
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+                foreach (Match item in matches)
+                {
+                    string leftWord = item.Groups[2].Value;
+                    string rightWordReversed = ReversedWord(item.Groups[3].Value);
+                    if (leftWord == rightWordReversed)
+                    {
+                        string valid = $"{leftWord} <=> {item.Groups[3].Value}";
+                        validMatches.Add(valid);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+
+            if (validMatches.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+
+                Console.WriteLine(string.Join(", ", validMatches));
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+        }
+        static string ReversedWord(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                sb.Append(text[i]);
+            }
+            string reversed = sb.ToString();
+
+            return reversed;
+        }
+
+    }
+}
