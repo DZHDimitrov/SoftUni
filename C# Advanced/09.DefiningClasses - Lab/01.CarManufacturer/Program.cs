decb719aa1aae4d36c4b84d1db325ae3eb59b0cf
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-01 23:50:18 +0300

    Added DefiningClasses - Lab

---
+++ C# Advanced/09.DefiningClasses - Lab/01.CarManufacturer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 2009;
+        }
+    }
+}
