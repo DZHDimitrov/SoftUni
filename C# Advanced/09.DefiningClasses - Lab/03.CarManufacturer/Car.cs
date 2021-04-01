78cc97eead6f4bce80ce790c2adc654782d6bfbe
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-01 23:55:25 +0300

    Added DefiningClasses - Lab

---
+++ C# Advanced/09.DefiningClasses - Lab/03.CarManufacturer/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make,model,year)
+        {
+            FuelConsumption = fuelConsumption;
+            FuelQuantity = fuelQuantity;
+        }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double result = distance * FuelConsumption;
+            if (FuelQuantity - result > 0)
+            {
+
+                FuelQuantity -= result;
+                Console.WriteLine("Good Ride!!!");
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {Make} Model: {Model} Year: {Year} Fuel: {FuelQuantity:F2}";
+        }
+    }
+}
---
+++ C# Advanced/09.DefiningClasses - Lab/04.CarManufacturer/04.CarManufacturer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double quantity = double.Parse(Console.ReadLine());
+            double consumption = double.Parse(Console.ReadLine());
+            Engine engine1 = new Engine(524,1000);
+            Engine engine2 = new Engine(450, 890);
+            Tire[] tires = new Tire[4] { new Tire(1, 2.2), new Tire(1, 2.2), new Tire(1, 2.2), new Tire(1, 2.2) };
+
+            Car car = new Car(make, model, year, quantity, consumption, engine1,tires);
+
+            Console.WriteLine($"{car.Make} {car.Model} {car.Year} {car.FuelQuantity} {car.FuelConsumption} {car.Engine.HorsePower} {car.Engine.CubicCapacity} {car.Tires.Length}");
+                  
+        }
+    }
+}
