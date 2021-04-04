8bd0307d1160e0d194be4566e479a4ac0eecc7f6
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-04 23:27:45 +0300

    Added Generics - Lab

---
+++ C# Advanced/11.Generics - Lab/Task1/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(5);
+            box.Add(5);
+            box.Add(5);
+            box.Remove();
+            box.Remove();
+            Console.WriteLine(box.Count);
+        }
+    }
+}
