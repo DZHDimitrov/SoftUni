47e93d2b9cc4aea41a449967390d8b6dcee2c5ee
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-04 23:31:50 +0300

    Added Generics - Lab

---
+++ C# Advanced/11.Generics - Lab/02.ArrayCreator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            var array = ArrayCreator.Create(10, 1.5);
+            Console.WriteLine(string.Join(" ", array));
+            
+        }
+    }
+}
