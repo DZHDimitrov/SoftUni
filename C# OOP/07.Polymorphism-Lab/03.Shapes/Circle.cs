d1b16fa5c8cdabef18e2b5884d306b9657272912
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-16 23:44:54 +0300

    Added Polymorphism - Lab

---
+++ C# OOP/07.Polymorphism-Lab/03.Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.Radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
