3dc64b326539661597ef57b46a919197ed6c0173
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-19 23:54:43 +0300

    Added Reflection - Lab

---
+++ C# OOP/09.Reflection-Lab/01.Stealer/Spy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, string[] fields)
+        {
+
+            Type currentType = Type.GetType(className);
+
+            FieldInfo[] infoaboutfields = currentType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(currentType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {currentType}");
+
+            foreach (var item in infoaboutfields.Where(x => fields.Contains(x.Name)))
+            {
+                sb.AppendLine($"{item.Name} = {item.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
