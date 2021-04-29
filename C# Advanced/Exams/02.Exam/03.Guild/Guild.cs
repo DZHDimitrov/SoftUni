b12d876a721b9312b02436dbea9fc87cfa3f5cde
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-04-29 14:21:47 +0300

    Add Csharp-Advanced Exams

---
+++ C# Advanced/Exams/02.Exam/03.Guild/Guild.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private IList<Player> roster;
+
+        public Guild(string name, int capacity)
+        {
+            this.roster = new List<Player>();
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return this.roster.Count;
+            }
+        }
+
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            return roster.Remove(roster.FirstOrDefault(x => x.Name == name));
+        }
+
+       public void PromotePlayer(string name)
+        {
+            var player = roster.FirstOrDefault(x => x.Name == name);
+            if (player != null && player.Rank != "Member")
+            {
+                player.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            var player = roster.FirstOrDefault(x => x.Name == name);
+            if (player != null && player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            Player[] players = this.roster.Where(x => x.Class == @class).ToArray();
+
+            for (int i = 0; i < this.roster.Count; i++)
+            {
+                if (this.roster[i].Class == @class)
+                {
+                    this.roster.Remove(this.roster[i]);
+                    i--;
+                }
+            }
+
+            return players;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {Name}");
+
+            foreach (var player in roster)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        
+    }
+}
