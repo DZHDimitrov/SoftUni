13b1effe3b7c39d32f115e6505163d9e00a8d740
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-06-09 00:04:20 +0300

    Add lastProblem from StreamsAndFiles - Exercise

---
+++ C# Advanced/15.StreamsAndFiles - Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string extractPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),"exercise.zip");
+            ZipFile.CreateFromDirectory("../../../Files", extractPath);
+
+            string resultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),"MyResult");
+            Directory.CreateDirectory(resultPath);
+            ZipFile.ExtractToDirectory(extractPath, resultPath);
+
+        }
+    }
+}
