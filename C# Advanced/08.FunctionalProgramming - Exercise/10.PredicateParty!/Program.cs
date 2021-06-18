950f67949566c64e81c6cd87f7317b4b6496416b
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-06-18 17:24:31 +0300

    Add solutions for PredicateParty and TriFunction to FunctionalProgramming - Exercises

---
+++ C# Advanced/08.FunctionalProgramming - Exercise/10.PredicateParty!/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.PredicateParty_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Func<string, string, string, List<string>, List<string>> partyGuests = (a, b, c, guests) =>
+            {
+                if (a == "Remove")
+                {
+                    return b == "StartsWith" ? guests.Where(x => !x.StartsWith(c)).ToList() :
+                    b == "EndsWith" ? guests.Where(x => !x.EndsWith(c)).ToList() :
+                    b == "Length" ? guests.Where(x => x.Length != int.Parse(c)).ToList() : null;
+                }
+                else if (a == "Double")
+                {
+                    List<string> people = b == "StartsWith" ? guests.Where(x => x.StartsWith(c)).ToList() :
+                    b == "EndsWith" ? guests.Where(x => x.EndsWith(c)).ToList() :
+                    b == "Length" ? guests.Where(x => x.Length == int.Parse(c)).ToList() :
+                    null;
+
+                    foreach (var person in people)
+                    {
+                        var index = guests.IndexOf(person);
+                        guests.Insert(index + 1, person);
+                    }
+                    return guests;
+                }
+                return null;
+            };
+            string line = Console.ReadLine();
+
+            while (line != "Party!")
+            {
+                string[] cmdArgs = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = cmdArgs[0];
+                string criteriaOne = cmdArgs[1];
+                string criteriaTwo = cmdArgs[2];
+                guests = partyGuests(command, criteriaOne, criteriaTwo, guests);
+
+                line = Console.ReadLine();
+            }
+            if (guests.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", guests) + " " + "are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+    }
+}
---
+++ C# Advanced/08.FunctionalProgramming - Exercise/12.TriFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Func<string, int, bool> myFunc = (name, length) =>
+            {
+                int counter = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    counter += name[i];
+                }
+                if (counter >= length)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            foreach (var name in names)
+            {
+                if (myFunc(name, number))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            }
+        }
+    }
+}
