907eda5617aabbc21ee667c638a22b8d5f3d00e3
Daniel Dimitrov <77573390+DZHDimitrov@users.noreply.github.com>
2021-10-05 21:55:08 +0300

    Update 05.SubqueriesAndJoins.sql

--- C# Databases Basics/05.SubqueriesAndJoins/05.SubqueriesAndJoins.sql
+++ C# Databases Basics/05.SubqueriesAndJoins/05.SubqueriesAndJoins.sql
@@ -159,3 +159,40 @@ SELECT ContinentCode,
 WHERE CurrencyRank =1
 ORDER BY ContinentCode
 
+--16. Countries without any mountains
+SELECT COUNT(*) AS Count FROM Countries AS c
+	LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+	WHERE mc.MountainId IS NULL
+	
+--17. Highest Peak and Longest River by country
+SELECT TOP(5)
+	c.CountryName,
+	MAX(p.Elevation) AS HighestPeakElevation,
+	MAX(r.Length) AS LongestRiverLength 
+FROM Countries AS c
+	LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+	LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+	LEFT JOIN Peaks AS p ON m.Id = p.MountainId
+	LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+	LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+	GROUP BY c.CountryName
+	ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC,CountryName ASC
+
+--18. Highest PeakName and Elevation by country
+SELECT TOP(5)
+	CountryName AS Country,
+	ISNULL(PeakName,'(no highest peak)')AS [Highest Peak Name],
+	ISNULL(Elevation,0) AS [Highest Peak Elevation],
+	ISNULL(MountainRange,'(no mountain)') AS Mountain 
+FROM (	
+	SELECT c.CountryName,
+		   p.PeakName,
+		   p.Elevation,
+		   m.MountainRange,
+		   DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS PeakRank
+	FROM Countries AS c
+	LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+	LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+	LEFT JOIN Peaks AS p ON m.Id = p.MountainId) AS PeaksRankingSubquery
+WHERE PeakRank = 1
+ORDER BY Country,[Highest Peak Name]
