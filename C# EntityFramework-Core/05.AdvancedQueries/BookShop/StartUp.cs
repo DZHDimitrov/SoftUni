0eccc2731f7f29f5ba3cc0cedcd0f8850d502ab1
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-11-08 23:58:38 +0200

    Add AdvancedQueries - Exercise

---
+++ C# EntityFramework-Core/05.AdvancedQueries/BookShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            var result = GetGoldenBooks(db);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+            var sb = new StringBuilder();
+            var books = context
+                .Books
+                .Where(x => x.AgeRestriction == ageRestriction)
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+
+            return string.Join(Environment.NewLine, books);
+        }
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+            var books = context
+                .Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books);
+        }
+    }
+}
