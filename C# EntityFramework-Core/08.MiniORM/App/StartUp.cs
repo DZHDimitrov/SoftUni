e7f8de83f918a0f7a798f505cd59ba09a0778edb
DZHDimitrov <ddreamzdimitrov@abv.bg>
2021-12-07 20:11:14 +0200

    Add MiniORM - exercise

---
+++ C# EntityFramework-Core/08.MiniORM/App/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace ORM_Fundamentals
+{
+    using ORM_Fundamentals.App.Data;
+    using ORM_Fundamentals.App.Data.Entities;
+    using System.Linq;
+
+    class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniDbContext db = new SoftUniDbContext("Server=.\\SQLEXPRESS;Database=SoftUni;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            db.Employees.Add(new Employee("Gosho", "Inserted", db.Departments.First().Id, true));
+
+            Employee employee = db.Employees.Last();
+            employee.FirstName = "Modified";
+
+            db.SaveChanges();
+        }
+    }
+}
---
+++ C# EntityFramework-Core/08.MiniORM/MiniORM/ConnectionManager.cs
@@ -0,0 +1,20 @@
+﻿namespace ORM_Fundamentals.MiniORM
+{
+    using System;
+    internal class ConnectionManager : IDisposable
+	{
+		private readonly DatabaseConnection connection;
+
+		public ConnectionManager(DatabaseConnection connection)
+		{
+			this.connection = connection;
+
+			this.connection.Open();
+		}
+
+		public void Dispose()
+		{
+			this.connection.Close();
+		}
+	}
+}
