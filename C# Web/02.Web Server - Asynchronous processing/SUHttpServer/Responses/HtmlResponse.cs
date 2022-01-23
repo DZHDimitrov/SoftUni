7d77c7127f1d4d2c54f59be1e7dcaff4b4cdd896
ddreamz <ddreamzdimitrov@abv.bg>
2022-01-23 20:21:05 +0200

    Web Server - Asynchronous processing

---
+++ C# Web/02.Web Server - Asynchronous processing/SUHttpServer/Responses/HtmlResponse.cs
@@ -0,0 +1,10 @@
+﻿using SUHttpServer.HTTP;
+
+namespace SUHttpServer.Responses
+{
+    public class HtmlResponse : ContentResponse
+    {
+        public HtmlResponse(string text) : base(text,ContentType.Html)
+        {}
+    }
+}
---
+++ C# Web/02.Web Server - Asynchronous processing/SUHttpServer/Responses/UnauthorizedResponse.cs
@@ -0,0 +1,10 @@
+﻿using SUHttpServer.HTTP;
+
+namespace SUHttpServer.Responses
+{
+    public class UnauthorizedResponse : Response
+    {
+        public UnauthorizedResponse() : base(StatusCode.Unauthorized)
+        {}
+    }
+}
