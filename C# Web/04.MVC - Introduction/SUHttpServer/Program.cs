8305ff4c39cb91e1a44a314679d4f05a13bde55c
ddreamz <ddreamzdimitrov@abv.bg>
2022-02-01 23:14:59 +0200

    Add MVC architecture to custom web server.

---
+++ C# Web/04.MVC - Introduction/SUHttpServer/Program.cs
@@ -0,0 +1,3 @@
+﻿using SUHttpServer;
+
+await Startup.Main();
---
+++ C# Web/04.MVC - Introduction/SUHttpServer/Startup.cs
@@ -0,0 +1,32 @@
+﻿using SUHttpServer.Controllers;
+using SUHttpServer.HTTP;
+using SUHttpServer.Responses;
+using System.Text;
+using System.Web;
+
+namespace SUHttpServer
+{
+    public class Startup
+    {
+        
+        public static async Task Main()
+        {
+
+            var server = new HttpServer(routes => routes
+            .MapGet<HomeController>("/",c=> c.Index())
+            .MapGet<HomeController>("/Redirect", c => c.Redirect())
+            .MapGet<HomeController>("/HTML", c => c.Html())
+            .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+            .MapGet<HomeController>("/Content", c => c.Content())
+            .MapPost<HomeController>("/Content", c => c.DownloadContent())
+            .MapGet<HomeController>("/Cookies", c => c.Cookies())
+            .MapGet<HomeController>("/Session", c => c.Session())
+            .MapGet<UsersController>("/Login",c=> c.Login())
+            .MapPost<UsersController>("/Login",c=>c.LogInUser())
+            .MapGet<UsersController>("/Logout",c=>c.Logout())
+            .MapGet<UsersController>("/UserProfile",c=>c.GetUserData())
+            );
+            await server.Start();
+        }
+    }
+}
