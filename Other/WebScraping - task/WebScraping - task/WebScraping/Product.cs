dd8fe1a8a6990fcd8abfeab96c32b6a09d356912
ddreamz <ddreamzdimitrov@abv.bg>
2022-05-11 14:49:09 +0300

    Add web-scraping task(to be deleted).

---
+++ Other/WebScraping - task/WebScraping - task/WebScraping/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebScraping
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+
+        public string? Price { get; set; }
+
+        public decimal? Rating { get; set; }
+    }
+}
